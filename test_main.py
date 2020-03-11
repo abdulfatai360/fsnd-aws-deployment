f92eeda3e710b64c2d89bb449af291c967d2c767
Abdulfatai Jimoh <abdulfatai360@gmail.com>
2020-03-12 00:28:52 +0100

    make fix

--- test_main.py
+++ test_main.py
@@ -5,12 +5,14 @@ import os
 import json
 import pytest
 
+
 import main
 
 SECRET = 'TestSecret'
 TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjEzMDY3OTAsIm5iZiI6MTU2MDA5NzE5MCwiZW1haWwiOiJ3b2xmQHRoZWRvb3IuY29tIn0.IpM4VMnqIgOoQeJxUbLT-cRcAjK41jronkVrqRLFmmk'
 EMAIL = 'wolf@thedoor.com'
 PASSWORD = 'huff-puff'
+
 
 @pytest.fixture
 def client():
@@ -19,7 +21,6 @@ def client():
     client = main.APP.test_client()
 
     yield client
-
 
 
 def test_health(client):
@@ -31,7 +32,7 @@ def test_health(client):
 def test_auth(client):
     body = {'email': EMAIL,
             'password': PASSWORD}
-    response = client.post('/auth', 
+    response = client.post('/auth',
                            data=json.dumps(body),
                            content_type='application/json')
 
